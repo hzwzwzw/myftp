eb7a55029606164199ca4555a60ff9d6c66ce904
huangzewen <zw-huang22@mails.tsinghua.edu.cn>
2024-10-06 18:53:42 +0800

    add USER, PASS

--- tcp/client.c
+++ tcp/client.c
@@ -10,6 +10,9 @@
 
 int main(int argc, char **argv)
 {
+	printf("*****************\n");
+	printf("*FTP CLIENT VIEW*\n");
+	printf("*****************\r\n");
 	int sockfd;
 	struct sockaddr_in addr;
 	char sentence[8192];
@@ -39,58 +42,63 @@ int main(int argc, char **argv)
 		printf("Error connect(): %s(%d)\n", strerror(errno), errno);
 		return 1;
 	}
-
-	// 获取键盘输入
-	fgets(sentence, 4096, stdin);
-	len = strlen(sentence);
-	sentence[len] = '\n';
-	sentence[len + 1] = '\0';
-
-	// 把键盘输入写入socket
-	p = 0;
-	while (p < len)
-	{
-		int n = write(sockfd, sentence + p, len + 1 - p); // write函数不保证所有的数据写完，可能中途退出
-		if (n < 0)
-		{
-			printf("Error write(): %s(%d)\n", strerror(errno), errno);
-			return 1;
-		}
-		else
-		{
-			p += n;
-		}
-	}
-
-	// 榨干socket接收到的内容
-	p = 0;
 	while (1)
 	{
-		int n = read(sockfd, sentence + p, 8191 - p);
-		if (n < 0)
+		// 榨干socket接收到的内容
+		p = 0;
+		while (1)
 		{
-			printf("Error read(): %s(%d)\n", strerror(errno), errno); // read不保证一次读完，可能中途退出
-			return 1;
-		}
-		else if (n == 0)
-		{
-			break;
-		}
-		else
-		{
-			p += n;
-			if (sentence[p - 1] == '\n')
+			int n = read(sockfd, sentence + p, 8191 - p);
+			if (n < 0)
+			{
+				printf("Error read(): %s(%d)\r\n", strerror(errno), errno); // read不保证一次读完，可能中途退出
+				return 1;
+			}
+			else if (n == 0)
 			{
 				break;
 			}
+			else
+			{
+				p += n;
+				while (sentence[p - 1] == 0)
+				{
+					p--;
+				}
+				if (sentence[p - 1] == '\n')
+				{
+					break;
+				}
+			}
+		}
+
+		// 注意：read并不会将字符串加上'\0'，需要手动添加
+		sentence[p - 1] = '\0';
+
+		printf("FROM SERVER: %s \r\n", sentence);
+
+		// 获取键盘输入
+		fgets(sentence, 4096, stdin);
+		len = strlen(sentence);
+		sentence[len] = '\n';
+		sentence[len + 1] = '\0';
+
+		// 把键盘输入写入socket
+		p = 0;
+		while (p < len)
+		{
+			int n = write(sockfd, sentence + p, len + 1 - p); // write函数不保证所有的数据写完，可能中途退出
+			if (n < 0)
+			{
+				printf("Error write(): %s(%d)\r\n", strerror(errno), errno);
+				return 1;
+			}
+			else
+			{
+				p += n;
+			}
 		}
 	}
-
-	// 注意：read并不会将字符串加上'\0'，需要手动添加
-	sentence[p - 1] = '\0';
-
-	printf("FROM SERVER: %s", sentence);
-
 	close(sockfd);
 
 	return 0;
--- tcp/server.c
+++ tcp/server.c
@@ -9,10 +9,162 @@
 #include <memory.h>
 #include <stdio.h>
 
+#define STATUS_user_not_logged_in 0
+#define STATUS_user_need_password 1
+#define STATUS_user_logged_in 2
+
+int status_user = STATUS_user_not_logged_in;
+char *user_name;
+char *user_email;
+
+int listenfd, connfd; // 监听socket和连接socket不一样，后者用于数据传输
+struct sockaddr_in addr;
+
+int readMsg(int fd, char *sentence, int *len)
+{
+	// 榨干socket传来的内容
+	int p = 0;
+	while (1)
+	{
+		int n = read(fd, sentence + p, 8191 - p);
+		if (n < 0)
+		{
+			printf("Error read(): %s(%d)\r\n", strerror(errno), errno);
+			close(fd);
+			continue;
+		}
+		else if (n == 0)
+		{
+			break;
+		}
+		else
+		{
+			p += n;
+			while (sentence[p - 1] == 0)
+			{
+				p--;
+			}
+			if (sentence[p - 1] == '\n')
+			{
+				break;
+			}
+		}
+	}
+	// socket接收到的字符串并不会添加'\0'
+	sentence[p - 1] = '\0';
+	if (len != NULL)
+		*len = p - 1;
+}
+
+int writeMsg(int fd, char *sentence, int len)
+{
+	if (len == 0)
+		len = strlen(sentence);
+	// 发送字符串到socket
+	int p = 0;
+	while (p < len)
+	{
+		int n = write(fd, sentence + p, len + 1 - p);
+		if (n < 0)
+		{
+			printf("Error write(): %s(%d)\r\n", strerror(errno), errno);
+			return 1;
+		}
+		else
+		{
+			p += n;
+		}
+	}
+}
+
+int strip(char *sentence, int len)
+{
+	if (len == 0)
+		len = strlen(sentence);
+	int p = len - 1;
+	while (sentence[p] == ' ' || sentence[p] == '\n' || sentence[p] == '\r')
+	{
+		p--;
+	}
+	sentence[p + 1] = '\0';
+	return p + 1;
+}
+
+int arguments_break(char *arguments, char *next)
+{
+	char *p = arguments;
+	int count = 0;
+	while (*p != '\0')
+	{
+		if (*p == ' ')
+			count++;
+		p++;
+	}
+	if (count == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		strncpy(next, arguments, p - arguments);
+		return p - arguments;
+	}
+}
+
+int proc_USER(char *arguments)
+{
+	if (status_user != STATUS_user_not_logged_in)
+	{
+		writeMsg(connfd, "503 Bad sequence of commands.\n", 0);
+	}
+	char next[256];
+	int len = arguments_break(arguments, next);
+	if (len != 0)
+	{
+		writeMsg(connfd, "501 Syntax error in parameters or arguments.\n", 0);
+	}
+	else
+	{
+		if (strcmp(arguments, "anonymous") == 0)
+		{
+			status_user = STATUS_user_need_password;
+			user_name = malloc(10 * sizeof(char));
+			strcpy(user_name, "anonymous");
+			writeMsg(connfd, "331 Guest login ok, send your complete e-mail address as password.\n", 0);
+		}
+		else
+		{
+			// user not exist
+			writeMsg(connfd, "530 It is not a valid user.\n", 0);
+		}
+	}
+}
+
+int proc_PASS(char *arguments)
+{
+	if (status_user != STATUS_user_need_password)
+	{
+		writeMsg(connfd, "503 Bad sequence of commands.\n", 0);
+	}
+	else if (status_user == STATUS_user_need_password)
+	{
+		if (strcmp(user_name, "anonymous") == 0)
+		{
+			user_email = malloc(strlen(arguments) * sizeof(char));
+			strcpy(user_email, arguments);
+			status_user = STATUS_user_logged_in;
+			char welcome[512];
+			sprintf(welcome, "230-\r\n230-Welcome to\r\n230- School of Software\r\n230- FTP Archives at ftp.ssast.org\r\n230-\r\n230-This site is provided as a public service by School of\r\n230-Software. Use in violation of any applicable laws is strictly\r\n230-prohibited. We make no guarantees, explicit or implicit, about the\r\n230-contents of this site. Use at your own risk.\r\n230-\r\n230 Guest login ok, access restrictions apply.\r\n");
+			writeMsg(connfd, welcome, 0);
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
-	int listenfd, connfd; // 监听socket和连接socket不一样，后者用于数据传输
-	struct sockaddr_in addr;
+	printf("*****************\n");
+	printf("*FTP SERVER VIEW*\n");
+	printf("*****************\r\n");
 	char sentence[8192];
 	int p;
 	int len;
@@ -20,7 +172,7 @@ int main(int argc, char **argv)
 	// 创建socket
 	if ((listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
 	{
-		printf("Error socket(): %s(%d)\n", strerror(errno), errno);
+		printf("Error socket(): %s(%d)\r\n", strerror(errno), errno);
 		return 1;
 	}
 
@@ -33,14 +185,14 @@ int main(int argc, char **argv)
 	// 将本机的ip和port与socket绑定
 	if (bind(listenfd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
 	{
-		printf("Error bind(): %s(%d)\n", strerror(errno), errno);
+		printf("Error bind(): %s(%d)\r\n", strerror(errno), errno);
 		return 1;
 	}
 
 	// 开始监听socket
 	if (listen(listenfd, 10) == -1)
 	{
-		printf("Error listen(): %s(%d)\n", strerror(errno), errno);
+		printf("Error listen(): %s(%d)\r\n", strerror(errno), errno);
 		return 1;
 	}
 
@@ -50,41 +202,46 @@ int main(int argc, char **argv)
 		// 等待client的连接 -- 阻塞函数
 		if ((connfd = accept(listenfd, NULL, NULL)) == -1)
 		{
-			printf("Error accept(): %s(%d)\n", strerror(errno), errno);
+			printf("Error accept(): %s(%d)\r\n", strerror(errno), errno);
 			continue;
 		}
 
+		writeMsg(connfd, "220 ftp.ssast.org FTP server ready.\n", 0);
+
 		while (1) // 每次处理一条语句
 		{
-			// 榨干socket传来的内容
-			p = 0;
-			while (1)
-			{
-				int n = read(connfd, sentence + p, 8191 - p);
-				if (n < 0)
-				{
-					printf("Error read(): %s(%d)\n", strerror(errno), errno);
-					close(connfd);
-					continue;
-				}
-				else if (n == 0)
-				{
-					break;
-				}
-				else
-				{
-					p += n;
-					if (sentence[p - 1] == '\n')
-					{
-						break;
-					}
-				}
-			}
-			// socket接收到的字符串并不会添加'\0'
-			sentence[p - 1] = '\0';
-			len = p - 1;
-
+			readMsg(connfd, sentence, &len);
 			// TODO: 处理语句
+			printf("Received: %s\r\n", sentence);
+			char command[5];
+			char argument[256];
+			strncpy(command, sentence, 4);
+			if (command[3] == ' ')
+			{
+				command[3] = '\0';
+				if (len > 4)
+					strncpy(argument, sentence + 4, len - 4);
+			}
+			else
+			{
+				command[4] = '\0';
+				if (len > 5)
+					strncpy(argument, sentence + 5, len - 5);
+			}
+			strip(argument, 0);
+			if (!strcmp(command, "USER"))
+			{
+				proc_USER(argument);
+			}
+			else if (!strcmp(command, "PASS"))
+			{
+				proc_PASS(argument);
+			}
+			else
+			{
+				printf("Unknown command: %s\r\n", command);
+				writeMsg(connfd, "500 Unknown command.\n", 0);
+			}
 		}
 
 		// // 字符串处理
